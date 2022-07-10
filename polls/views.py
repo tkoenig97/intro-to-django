ac557d8d277248398d72296eca569bb762ce85a0
tkoenig97 <tylerskoenig@gmail.com>
2022-07-10 14:49:11 -0500

    Added templates directory

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 # Create your views here.
@@ -18,6 +18,9 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id) 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('=pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
