51cc34b3a8563c38b48ef4a4d15eca3b65df80f4
tkoenig97 <tylerskoenig@gmail.com>
2022-07-10 14:41:49 -0500

    Added index view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id) 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('=pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
